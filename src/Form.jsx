9b684eb487e6f6e8c29d50127747bd46fde5009a
Archana MohanDas <archanamdasofficial@gmail.com>
2025-08-01 12:55:29 +0530

    First

---
+++ src/Form.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react"
+
+const Form = () => {
+
+    const [formData,setformData] = useState({
+        firstName : "",
+        lastName : ""
+    })
+
+    const [submitted ,setSubmitted] = useState(false)
+
+    const handleChange = (e) => {
+
+        const {name,value} = e.target
+        setformData((prev) => ({ ...prev , [name] : value }))
+        
+    }
+
+    const handleSubmit = (e) => {
+
+        e.preventDefault()
+        setSubmitted(true)
+     
+    }
+
+    
+
+    return (
+        <div>
+            <h1> Full Name Display </h1>
+
+            <form onSubmit={handleSubmit} className="formStyle">
+                <div>
+                    <label htmlFor="firstName">First Name:</label>
+                    <input type="text"
+                     id="firstName"
+                     name="firstName"
+                     value={formData.firstName}
+                     onChange={handleChange}
+                     required
+                     />
+                </div>
+
+                <div>  
+                    <label htmlFor="lastName">Last Name:</label>
+                    <input type="text" 
+                    id="lastName" 
+                    name="lastName"
+                    value={formData.lastName} 
+                    onChange={handleChange}
+                    required
+                    />
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+
+            {
+                submitted && (
+                    <h3> Full Name : {formData.firstName} {formData.lastName} </h3>                )
+            }
+
+        </div>
+    )
+
+}
+
+export default Form
